538e74c9f003eaadde4294f8353de7e68985ef6b
jkdamewo <jkdamewo@mit.edu>
2023-08-15 18:44:45 -0400

    Added per-site CGCNN

--- cgcnn/model.py
+++ cgcnn/model.py
@@ -186,6 +186,91 @@ class CrystalGraphConvNet(nn.Module):
                       for idx_map in crystal_atom_idx]
         return torch.cat(summed_fea, dim=0)
 
+
+class PerSiteCGCNet(nn.Module):
+    """
+    Create a crystal graph convolutional neural network for predicting per-site
+    material properties.
+    """
+    def __init__(self, orig_atom_fea_len, nbr_fea_len,
+                 atom_fea_len=64, n_conv=3, h_fea_len=128, n_h=1,
+                 classification=False):
+        """
+        Initialize CrystalGraphConvNet.
+        Parameters
+        ----------
+        orig_atom_fea_len: int
+          Number of atom features in the input.
+        nbr_fea_len: int
+          Number of bond features.
+        atom_fea_len: int
+          Number of hidden atom features in the convolutional layers
+        n_conv: int
+          Number of convolutional layers
+        h_fea_len: int
+          Number of hidden features after pooling
+        n_h: int
+          Number of hidden layers after pooling
+        """
+
+        super(PerSiteCGCNet, self).__init__()
+        self.classification = classification
+        self.embedding = nn.Linear(orig_atom_fea_len, atom_fea_len)
+        self.convs = nn.ModuleList([ConvLayer(atom_fea_len=atom_fea_len,
+                                    nbr_fea_len=nbr_fea_len)
+                                    for _ in range(n_conv)])
+        self.conv_to_fc = nn.Linear(atom_fea_len, h_fea_len)
+        self.conv_to_fc_softplus = nn.Softplus()
+        if n_h > 1:
+            self.fcs = nn.ModuleList([nn.Linear(h_fea_len, h_fea_len)
+                                      for _ in range(n_h-1)])
+            self.softpluses = nn.ModuleList([nn.Softplus()
+                                             for _ in range(n_h-1)])
+
+        if self.classification:
+            self.fc_out = nn.Linear(h_fea_len, 2)
+        else:
+            self.fc_out = nn.Linear(h_fea_len, 1)
+        if self.classification:
+            self.logsoftmax = nn.LogSoftmax(dim=1)
+            self.dropout = nn.Dropout()
+
+    def forward(self, atom_fea, nbr_fea, nbr_fea_idx, crystal_atom_idx):
+        """
+        Forward pass
+        N: Total number of atoms in the batch
+        M: Max number of neighbors
+        N0: Total number of crystals in the batch
+        Parameters
+        ----------
+        atom_fea: Variable(torch.Tensor) shape (N, orig_atom_fea_len)
+          Atom features from atom type
+        nbr_fea: Variable(torch.Tensor) shape (N, M, nbr_fea_len)
+          Bond features of each atom's M neighbors
+        nbr_fea_idx: torch.LongTensor shape (N, M)
+          Indices of M neighbors of each atom
+        crystal_atom_idx: list of torch.LongTensor of length N0
+          Mapping from the crystal idx to atom idx
+        Returns
+        -------
+        prediction: nn.Variable shape (N, sites_in_crystal)
+          Atom hidden features after convolution
+        """
+
+        atom_fea = self.embedding(atom_fea)
+        for conv_func in self.convs:
+            atom_fea = conv_func(atom_fea, nbr_fea, nbr_fea_idx)
+
+        atom_fea = self.conv_to_fc(self.conv_to_fc_softplus(atom_fea))
+        atom_fea = self.conv_to_fc_softplus(atom_fea)
+        if hasattr(self, 'fcs') and hasattr(self, 'softpluses'):
+            for fc, softplus in zip(self.fcs, self.softpluses):
+                atom_fea = softplus(fc(atom_fea))
+        out = self.fc_out(atom_fea)
+        out = [out[idx_map] for idx_map in crystal_atom_idx]
+
+        return out
+
 class Normalizer(object):
     """Normalize a Tensor and restore it later. """
 
